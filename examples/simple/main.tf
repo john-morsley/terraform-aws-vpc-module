89b74f00ef155b37c04b3797b538c33537fd0b94
John Morsley <johnmorsley@gmail.com>
2020-05-02 19:13:02 +0100

    Refactoring

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,15 +1,25 @@
-﻿module "vpc" {
-  
-  source = "../../"
-  
+#       _____ _                 _      
+#      / ____(_)               | |     
+#     | (___  _ _ __ ___  _ __ | | ___ 
+#      \___ \| | '_ ` _ \| '_ \| |/ _ \
+#      ____) | | | | | | | |_) | |  __/
+#     |_____/|_|_| |_| |_| .__/|_|\___|
+#                        | |           
+#                        |_|           
+
+module "vpc" {
+
+  source = "./../../../terraform-aws-vpc"
+  #source = "john-morsley/terraform-aws-vpc"
+
   name = "simple-example"
 
   cidr_block = "10.0.0.0/16" # 65,531 (65,536 possible - 5 reserved by AWS)
 
-  public_subnets  = ["10.0.0.0/24"] # 251 (256 possible - 5 reserved by AWS)
-  
+  public_subnets = ["10.0.0.0/24"] # 251 (256 possible - 5 reserved by AWS)
+
   tags = {
     Owner = "john-doe"
   }
-  
+
 }
