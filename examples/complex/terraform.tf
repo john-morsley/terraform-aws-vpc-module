c07adeffa3aabc7c215bbc21c359b7b20f2380df
John Morsley <johnmorsley@gmail.com>
2020-11-10 20:19:37 +0000

    Required providers

--- examples/complex/terraform.tf
+++ examples/complex/terraform.tf
@@ -10,6 +10,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
   
--- terraform.tf
+++ terraform.tf
@@ -10,8 +10,10 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
 
   required_version = ">= 0.13"
+
 }
